a39fa1d70afb1da996e2cf588c3f4afbbd1a018d
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-16 17:07:42 +0100

    some change

--- js/app.js
+++ js/app.js
@@ -4,10 +4,57 @@ const app = new Vue({
     activeIndex: 0,
     search: '',
     userLogged: {
-      name: 'Sofia',
+      name: 'Salvatore',
       text: 'Ultimo messaggio inviato',
-      img: 'img/avatar_1.jpg',
+      img: 'img/avatar_8.jpg',
     },
+    users: [
+      {
+        name: 'Fabio',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_3.jpg',
+      },
+      {
+        name: 'Michele',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_2.jpg',
+      },
+      {
+        name: 'Alessandro L.',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_4.jpg',
+      },
+      {
+        name: 'Alessandro B.',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_8.jpg',
+      },
+      {
+        name: 'Claudia',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_5.jpg',
+      },
+      {
+        name: 'Federico',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_6.jpg',
+      },
+      {
+        name: 'Davide',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_7.jpg',
+      },
+      {
+        name: 'Davide',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_7.jpg',
+      },
+      {
+        name: 'Davide',
+        text: 'Ultimo messaggio inviato',
+        img: 'img/avatar_7.jpg',
+      },
+    ],
     deskNotify: {
       title: 'Ricevi notifiche di nuovi messaggi',
       sub: 'Attiva notifiche desktop',
@@ -15,26 +62,18 @@ const app = new Vue({
     usersArray: [],
   },
   methods: {
-    fillArrays() {
-      users.forEach((user) => {
-        this.usersArray.push(user);
-      });
-    },
-
     filterUsers(search) {
       if (search == '') {
-        return this.usersArray;
+        return this.users;
       } else {
-        return this.usersArray.filter((user) => {
+        return this.users.filter((user) => {
           return user.name.toLowerCase().match(search.toLowerCase());
         });
       }
     },
+    // seleziona una chat da aprire
     selectedChat(index) {
       this.activeIndex = index;
     },
-  },
-  created() {
-    this.fillArrays();
   },
 });

99c55af1f891195488f5e9e6ce926ad5fe23aa4e
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-16 19:20:46 +0100

    save

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ const app = new Vue({
   data: {
     activeIndex: 0,
     search: '',
+    newMsg: {
+      date: '',
+      message: '',
+      sent: true,
+    },
     userLogged: {
       name: 'Salvatore',
       text: 'Ultimo messaggio inviato',
@@ -10,46 +15,53 @@ const app = new Vue({
     },
     users: [
       {
+        id: 0,
         name: 'Fabio',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_3.jpg',
       },
       {
+        id: 1,
         name: 'Michele',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_2.jpg',
+        messages: [
+          {
+            text: 'Lorem Lorem',
+            sent: true,
+          },
+          {
+            text: 'Lorem Lorem',
+            sent: false,
+          },
+        ],
       },
       {
+        id: 2,
         name: 'Alessandro L.',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_4.jpg',
       },
       {
+        id: 3,
         name: 'Alessandro B.',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_8.jpg',
       },
       {
+        id: 4,
         name: 'Claudia',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_5.jpg',
       },
       {
+        id: 5,
         name: 'Federico',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_6.jpg',
       },
       {
-        name: 'Davide',
-        text: 'Ultimo messaggio inviato',
-        img: 'img/avatar_7.jpg',
-      },
-      {
-        name: 'Davide',
-        text: 'Ultimo messaggio inviato',
-        img: 'img/avatar_7.jpg',
-      },
-      {
+        id: 6,
         name: 'Davide',
         text: 'Ultimo messaggio inviato',
         img: 'img/avatar_7.jpg',
@@ -62,6 +74,7 @@ const app = new Vue({
     usersArray: [],
   },
   methods: {
+    // filtro chat
     filterUsers(search) {
       if (search == '') {
         return this.users;
@@ -71,9 +84,16 @@ const app = new Vue({
         });
       }
     },
+    postSentMessage(newMessage) {
+      console.log(...this.newMessage);
+      this.newMessage = '';
+    },
+    postMessage(newMessage) {
+      this.users[activeIndex].messages.push(newMessage);
+    },
     // seleziona una chat da aprire
-    selectedChat(index) {
-      this.activeIndex = index;
+    selectedChat(key) {
+      this.activeIndex = key;
     },
   },
 });
